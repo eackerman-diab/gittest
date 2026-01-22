976f37d8e9c7893b2901a2bd97e9668963add6b8
eackerman-diab <eackerman-diab@users.noreply.github.com>
2026-01-21 19:36:54 -0700

    Add unit tests for Hello World application

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetGreeting());
+    }
+
+    public static string GetGreeting()
+    {
+        return "Hello, World!";
+    }
+}
